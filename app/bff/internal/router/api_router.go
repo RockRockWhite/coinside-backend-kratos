288cac8814b4266c0b234ad9d56ee6bced09596c
RockRockWhite <1834309151@qq.com>
2022-11-20 21:27:51 +0800

    feature: confirm is a member of a card when calling a api

--- app/bff/internal/router/api_router.go
+++ app/bff/internal/router/api_router.go
@@ -17,38 +17,6 @@ func NewApiRouter(userController *controller.UserController, cardController *con
 	router = UserRouter(router, userController)
 	router = CardRouter(router, cardController)
 	router = TeamRouter(router, teamController)
-	//user := router.Group("/users")
-	//{
-	//	user.GET("/:username", controllers.GetUser)
-	//	user.GET("", controllers.GetUsers)
-	//	user.GET("/count", controllers.CountUser)
-	//	user.POST("", controllers.AddUser)
-	//	user.PATCH(
-	//		"/:username",
-	//		middlewares.JwtAuth(middlewares.Role_Admin|middlewares.Role_Cond, func(c *gin.Context) bool {
-	//			username := c.Param("username")
-	//			claims := c.MustGet("claims").(*utils.JwtClaims)
-	//			return username == claims.Username
-	//		}), controllers.PatchUser)
-	//	user.PATCH(
-	//		"/:username/password",
-	//		middlewares.JwtAuth(middlewares.Role_Cond, func(c *gin.Context) bool {
-	//			username := c.Param("username")
-	//			claims := c.MustGet("claims").(*utils.JwtClaims)
-	//			return username == claims.Username
-	//		}), controllers.PatchPassword)
-	//	user.DELETE("/:username",
-	//		middlewares.JwtAuth(middlewares.Role_Admin|middlewares.Role_Cond, func(c *gin.Context) bool {
-	//			username := c.Param("username")
-	//			claims := c.MustGet("claims").(*utils.JwtClaims)
-	//			return username == claims.Username
-	//		}), controllers.DeleteUser)
-	//}
-	//
-	//token := router.Group("/tokens")
-	//{
-	//	token.POST("", controllers.CreateToken)
-	//}
 
 	return router
 }
